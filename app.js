d57192f14be5f1284d0b0b3eab8a8218ab20ce92
chenjyp <chenjyp@yonyou.com>
2019-12-24 19:24:07 +0800

    注册小程序和页面

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
+//注册小程序示例
 App({
-  //生命周期函数
+
+  //也可以传个参数
+  onLaunch: function (options) {
+    console.log('onLaunch');
+    console.log(options);
+
+    //网络请求
+
+    //获取用户数据
+
+  },
+
   /**
-   * 关闭后，小程序在后台会存活一段时间（两个小时），时间内打开不会执行初始化函数，还是会执行onShow，关闭执行onHide
-   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   * 当小程序启动，或从后台进入前台显示，会触发 onShow
    */
-  onLaunch: function () {
-    console.log('初始化：onLaunch');
-    //异步调用
+  onShow: function (options) {
+    console.log('onShow');
+    console.log(options);
+    //1.判断小程序的进入场景
+    switch(options.scene){
+      case 1001:;
+    }
+
+    //2.获取用户的信息，并且获取到用户信息之后，将用户的信息传递给服务器
     wx.getUserInfo({
-      //数据拿到之后再进行回调
       success:function(res){
         console.log(res);
       }
@@ -16,27 +32,21 @@ App({
   },
 
   /**
-   * 当小程序启动，或从后台进入前台显示，会触发 onShow
-   */
-  onShow: function (options) {
-    console.log('界面显示：onShow');
-    setTimeout(function(){
-      error = new Error();
-      throw error;
-    },3000);
-  },
-
-  /**
    * 当小程序从前台进入后台，会触发 onHide
    */
   onHide: function () {
-    console.log('关闭：onHide');
+
   },
 
   /**
    * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
    */
   onError: function (msg) {
-    console.log('出错：onError');
+
+  },
+
+  globalData:{
+    name:'globalName',
+    age:'22'
   }
 })
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,34 +1,77 @@
 // pages/home/home.js
+//getApp()获取App()产生的示例对象
+const app = getApp();
+const name = app.globalData.name;
+const age = app.globalData.age;
+
+//注册一个页面
+//页面有自己的生命周期函数
 Page({
+  //------ 2.初始化数据 ------
   data:{
-    name:'codeWhy',
-    age:18,
-    students:[
-      {id:110,name:'kobe',age:30},
-      { id: 111, name: 'iverson', age: 30 },
-      { id: 112, name: 'cater', age: 30 },
-      { id: 113, name: 't-mac', age: 30 },
-      { id: 114, name: 'lin', age: 30 }
-    ],
-    count:0
+    message:'哈哈哈',
+    list:''
   },
-  handleAddBtnClick(){
-    //1.错误做法，界面是不会刷新的
-    //this.data.count += 1
-    //console.log(this.data.count)
-    console.log('+按钮发生了点击');
-    this.setData({
-      count:this.data.count + 1
-    });
+
+  //------3.监听wxml中相关的事件 ------
+  handleGetUserInfo(event){
+    console.log(event);
+    console.log(name);
+    console.log(age);
+    console.log(app);
   },
-  handleSubBtnClick() {
-    console.log('-按钮发生了点击');
-    this.setData({
-      count: this.data.count - 1
-    });
+  handleViewClick(){
+    console.log("哈哈哈 被点击");
+  },
+
+  //------4.监听其它事件 ------
+  //监听页面的滚动
+  onPageScroll(obj){
+    //console.log(obj);
+  },
+  //监听页面滚动到底部
+  onReachBottom(){
+    console.log('页面到底部');
+  },
+  //监听页面下拉刷新
+  onPullDownRefresh() {
+    console.log('页面下拉刷新');
+  },
+  onTabItemTap(){
+    console.log('tabbar事件');
+  },
+
+  //------1.监听页面的生命周期函数 ------
+  //页面被加载出来
+  onLoad(){
+    var myThis = this;
+    console.log('onLoad');
+    wx.request({
+      url: 'http://123.207.32.32:8000/recommend',
+      //箭头函数的指针一层层向上找
+      success(res){
+        console.log(res);
+        const data = res.header;
+        myThis.setData({
+          list:data
+        });
+      }
+    })
+  },
+  //页面显示出来时
+  onShow(){
+    console.log('onShow');
+  },
+  //页面初次渲染完成时
+  onReady() {
+    console.log('onReady');
+  },
+  //页面隐藏
+  onHide(){
+    console.log('onHide');
+  },
+  
+  onUnload(){
+    console.log('onUnload');
   }
 })
-
-//编程范式
-//1.命令式编程：原生操作DOM
-//2.声明式编程：Vue/React/Angular
